2cecfc4e71e2247889a4859d43d0b8fc4f4fdf83
Tal Ben-Horin PhD <tbenhor@ncsu.edu>
2021-06-15 16:58:09 -0400

    Oysters!!!

---
+++ top10_genera.R
@@ -0,0 +1,78 @@
+rm(list=ls(all=TRUE))
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("phyloseq")
+BiocManager::install("dada2", version = "3.11")
+
+library(dada2); packageVersion("dada2")
+library(ape)
+library(gridExtra)
+library(knitr)
+library(phyloseq); packageVersion("phyloseq")
+library(Biostrings); packageVersion("Biostrings")
+library(ggplot2); packageVersion("ggplot2")
+library(scales)
+theme_set(theme_bw())
+
+seqtab <- readRDS("output/seqtab_final.rds")
+taxa <- readRDS("output/tax_final.rds")
+
+samples.out <- rownames(seqtab)
+
+sites <- read.csv("sites.csv", fill = FALSE, header = TRUE) 
+samdf <- data.frame(Site=sites$Site,Group=sites$Group,ID=sites$Sample) 
+rownames(samdf) <- samples.out
+
+## "Phyloseq" object from OTU table
+ps <- phyloseq(otu_table(seqtab, taxa_are_rows=FALSE), 
+               sample_data(samdf), 
+               tax_table(taxa))
+
+# Compute prevalence of each feature, store as data.frame
+prevdf = apply(X = otu_table(ps),
+               MARGIN = ifelse(taxa_are_rows(ps), yes = 1, no = 2),
+               FUN = function(x){sum(x > 0)})
+# Add taxonomy and total read counts to this data.frame
+prevdf = data.frame(Prevalence = prevdf,
+                    TotalAbundance = taxa_sums(ps),
+                    tax_table(ps))
+
+## Filtering
+#  Define prevalence threshold as 5% of total samples
+prevalenceThreshold = 0.05 * nsamples(ps)
+
+# Filter out prevalence
+keepTaxa = rownames(prevdf)[(prevdf$Prevalence >= prevalenceThreshold)]
+ps1 = prune_taxa(keepTaxa, ps)
+ps2 = tax_glom(ps1, "Genus", NArm = TRUE) #glom the pruned taxa 
+
+top10 <- names(sort(taxa_sums(ps2), decreasing=TRUE))[1:10]
+ps2.top10 <- transform_sample_counts(ps2, function(OTU) OTU/sum(OTU))
+ps2.top10 <- prune_taxa(top10, ps2.top10)
+
+ps2.top10.BS <- subset_samples(ps2.top10, Site=="BS")
+ps2.top10.SC <- subset_samples(ps2.top10, Site=="SC")
+ps2.top10.NR <- subset_samples(ps2.top10, Site=="NR")
+
+names <- taxa_names(ps2.top10.BS)
+
+p1 = plot_heatmap(ps2.top10.NR, taxa.label = "Genus", sample.label = "Group",low="white", high="#000033", 
+                  na.value = "white", sample.order = "Group",taxa.order = taxa_names(ps2.top10.BS),
+                  trans = identity_trans())
+p1 = p1  + theme(axis.text.x = element_text(size=8, angle=30, hjust=1, vjust=0.9),
+                  legend.title = element_text(size = 10)) +
+                  labs(fill = "Relative\nabundance")
+p2 = plot_heatmap(ps2.top10.SC, taxa.label = "Genus", sample.label = "Group",low="white", high="#000033", 
+                  na.value = "white", sample.order = "Group",taxa.order = taxa_names(ps2.top10.BS),
+                  trans = identity_trans())
+p2 = p2  + theme(axis.text.x = element_text(size=8, angle=30, hjust=1, vjust=0.9),
+                 legend.title = element_text(size = 10)) +
+                  labs(fill = "Relative\nabundance")
+p3 = plot_heatmap(ps2.top10.BS, taxa.label = "Genus", sample.label = "Group",low="white", high="#000033", 
+                  na.value = "white", sample.order = "Group",taxa.order = taxa_names(ps2.top10.BS),
+                  trans = identity_trans())
+p3 = p3 + theme(axis.text.x = element_text(size=8, angle=30, hjust=1, vjust=0.9),
+                  legend.title = element_text(size = 10)) +
+                  labs(fill = "Relative\nabundance")
+grid.arrange(p1, p2, p3, nrow = 1)
